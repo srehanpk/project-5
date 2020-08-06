e3b1053415a4bf3b8b12bb883175d25ed3b1aec3
srahmed <srehanpk@gmail.com>
2020-08-06 17:39:58 +0500

    make home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import useWebAnimations, {fadeInRight} from "@wellyshen/use-web-animations";
+import images from '../images/community1.png';
 
 function Home() {
+
+  const { ref } = useWebAnimations({...fadeInRight});
+ 
+  
+
   return (
     <div className="home">
-      <h1>Home Page</h1>
+     <section className="d-flex align-items-center mt-lg-5">
+     <div className="container-fluid nav_bg">
+     <div className="row">
+     <div className="col-10 mx-auto">
+
+     <div className="row">
+
+      <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+      <h1>Grow Your Business with <strong className="brand-name">IMPRESSION</strong> </h1>
+      <h6 className="my-3" >
+      We are the team of highly professional developers to make web and mobile applications with latest technologies </h6>
+
+      <div className="mt-3" >
+      <NavLink exact to="/service" className="btn-get-started nav-link">Get Started</NavLink>
+      </div>
+      </div>
+
+      <div className="col-lg-6 order-1 order-lg-2 mt-lg-5 mb-lg-5">
+      <img className="header-img" ref={ref} src={images} alt="header"/>
+      </div>
+
+     </div> 
+     </div>
+     </div>
+     </div>
+     </section>
     </div>
   );
 }
