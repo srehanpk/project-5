341651dc33751a600ecef28f9957acc4586f6442
srahmed <srehanpk@gmail.com>
2020-08-09 18:56:07 +0500

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -84,17 +84,6 @@ function Navbar() {
                       </NavLink>
                     </li>
                   </ul>
-                  <form className="d-flex">
-                    <input
-                      className="form-control mr-2"
-                      type="search"
-                      placeholder="Search"
-                      aria-label="Search"
-                    />
-                    <button className="btn btn-outline-success" type="submit">
-                      Search
-                    </button>
-                  </form>
                 </div>
               </div>
             </nav>
